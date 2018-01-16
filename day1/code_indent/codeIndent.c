f380ef6b8cd5de3cebb59ac2a2503431bd34cdbb
kryptokinght <minanshu.1998@gmail.com>
2018-01-16 08:53:43 +0530

    arranged the files in folders

--- day1/code_indent/codeIndent.c
+++ day1/code_indent/codeIndent.c
@@ -43,7 +43,7 @@ int main() {
       } 
       else if(c == '}')
         counter--;
-
+    	
       if(forw == '}' && c != '\n' && c != ' ') { //
       	printf("exec c=%c forw=%c prev=%c\n",c,forw,prev);
       	fprintf(fp,"\n");
