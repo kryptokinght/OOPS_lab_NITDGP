5d40f7ccc8c5c4b0d1ca8e2c7b59418ece78ae5d
kryptokinght <minanshu.1998@gmail.com>
2018-03-24 23:40:04 +0530

    add file to show how to write a c++ program

---
+++ extras/program_structure.cpp
@@ -0,0 +1,21 @@
+/*
+Dynamic binding,
+inheritance,
+polymorphism
+Message Communication: One object communicates with another,
+encapsulation: Object related data and functions are kept together,
+data abstraction: Hiding away the complex details and only showing the essential features 
+*/
+/*
+STRUCTURE OF A C++ PROGRAM
+0.Documentation
+1.preprocessor directive section
+	1. link section
+	2. definition section
+2.global variable declaration section
+3.classes and method definition section
+4.main function section
+	1.local variable declaration section
+	2.program statements
+5.method definition section(optional)
+*/
